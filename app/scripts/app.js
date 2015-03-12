11f64ac4935e3b6f8fb2535f1b52e2e02f38ff8b
Swagstopher <crystalcleare@gmail.com>
2015-03-12 06:56:01 -0700

    Saving Stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,7 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -37,3 +37,34 @@ angular
         redirectTo: '/'
       });
   });
+
+  Parse.initialize("iAkM7wKq1rqr60qYUUig2fbyNqoVmKgNo7S1pcd9", "7z1ZDoF1Sju2L5inrUCxWKxOIJjnCbVGUVO1Kqfb");
+
+  function register(){
+      var newUser = new Parse.User();
+
+      newUser.set("username", document.getElementById("registerEmail").value);
+      newUser.set("password", document.getElementById("registerPassword").value);
+      newUser.signUp(null, {
+          success: function(newUser)
+          {
+              // move user to next page to add additional info, in register.js
+          },
+          error: function(newUser, error){ // error if email is already taken
+              response.error("Error: " + error.code + " " + error.message);
+          }})
+  }
+
+  function logIn(){
+  var username = document.getElementById("loginEmail").value;
+  var password = document.getElementById("loginPassword").value;
+
+  Parse.User.logIn(username, password, {
+    success: function(user) {
+      // Do stuff after successful login.
+    },
+    error: function(user, error) {
+      // The login failed. Check error to see why.
+    }
+  })
+  }
